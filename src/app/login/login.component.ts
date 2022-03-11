90dcb8e761f382db095d9da626914493353872ec
kahlaoui-2020 <kahlaoug@gmail.com>
2022-03-11 22:02:44 +0100

    update UX  && Switch Theme

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { ChatService } from '../room/service/chat.service';
+import { AuthService } from './service/auth.service';
+import { PrimeNGConfig } from 'primeng/api';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-login',
@@ -9,13 +13,17 @@ import { AuthService } from './auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  
+  isChecked = true;
   loginForm!: FormGroup;
   constructor(
+    private chatService: ChatService, 
     private formBuilder: FormBuilder,
-    private auth: AuthService) { }
+    private auth: AuthService,
+    private primengConfig: PrimeNGConfig) { }
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.chatService.onDisconnect();
     this.loginForm = this.formBuilder.group({
       email: [null,[Validators.required, Validators.pattern('')]],
       password: [null, [Validators.required]]
@@ -25,6 +33,14 @@ export class LoginComponent implements OnInit {
   login(): void {
     this.auth.login(this.loginForm.value);
 
+  }
+  switchTheme(event: MatSlideToggleChange) {
+    if (event.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'light');
+  } 
   }
 
 }
