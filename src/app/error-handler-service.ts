907d40e3c85da4b3e5729d4a1af452c34219d82b
kahlaoui-2020 <kahlaoug@gmail.com>
2022-03-22 22:01:44 +0100

    create - Error Handler Service

---
+++ src/app/error-handler-service.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { ErrorModalComponent } from "./shared/error-modal/error-modal.component";
+
+@Injectable()
+export class ErrorHandlerService implements ErrorHandler {
+
+    constructor(
+        private injector: Injector,
+        private ngzone: NgZone) {}
+    handleError(error: any): void {
+        let dialog: MatDialog = this.injector.get(MatDialog)
+        if(error instanceof HttpErrorResponse) {
+            this.ngzone.run(() => {
+                dialog.open(ErrorModalComponent)
+            })
+            
+        } else {
+
+        }
+    }
+}
